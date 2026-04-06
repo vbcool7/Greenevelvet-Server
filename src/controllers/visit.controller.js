198da820bd75cbfd3030f7e0f26dc09c238c3611
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-04-06 11:49:40 +0530

    model control

--- src/controllers/visit.controller.js
+++ src/controllers/visit.controller.js
@@ -170,7 +170,7 @@ export const getVisitStats = async (request, response) => {
                     ],
 
                     smsClicks: [
-                        { $match: { type: "message_click" } },
+                        { $match: { type: "sms_click" } },
                         { $count: "count" },
                     ],
 
@@ -326,7 +326,7 @@ export const totalVisitStats = async (request, response) => {
                         { $count: "count" },
                     ],
                     smsClicks: [
-                        { $match: { type: "message_click" } },
+                        { $match: { type: "sms_click" } },
                         { $count: "count" },
                     ],
                     websiteClicks: [
