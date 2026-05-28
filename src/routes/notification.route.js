bff6050310487df9d26f674bb79ddf7eaa70b0c7
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-05-28 14:37:15 +0530

    update route

--- src/routes/notification.route.js
+++ src/routes/notification.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getMyNotifications, markNotificationAsRead } from "../controllers/notification.controller.js";
+import { protect } from "../middleware/auth.js";
 
 const notificationRouter = Router();
 
-notificationRouter.get("/my-notification", getMyNotifications);
-notificationRouter.patch("/mark-read", markNotificationAsRead);
+notificationRouter.get("/my-notification", protect(["Admin"]), getMyNotifications);
+notificationRouter.patch("/mark-read/:id", protect(["Admin"]), markNotificationAsRead);
 
 export default notificationRouter;
