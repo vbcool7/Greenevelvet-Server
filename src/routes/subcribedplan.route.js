785181c8450bde81493e0232146aa4f0de984729
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-08-21 18:29:36 +0530

    update route

--- src/routes/subcribedplan.route.js
+++ src/routes/subcribedplan.route.js
@@ -1,17 +1,28 @@
 import express from "express";
-import { Router } from "express";
-import { checkSubscription, createTransaction, nowPaymentsWebhook, } from "../controllers/subcribedplan.controller.js";
-import { protect } from "../middleware/auth.js";
+import {
+    Router
+} from "express";
+import {
+    checkSubscription,
+    createTransaction,
+    fetchEscortCurrentPlan,
+    nowPaymentsWebhook,
+} from "../controllers/subcribedplan.controller.js";
+import {
+    protect
+} from "../middleware/auth.js";
 
 const subcribedRouter = Router();
 
 subcribedRouter.post("/create-transaction", protect(["Escort"]), createTransaction);
 
-subcribedRouter.post("/nowpayments-webhook", express.raw({ type: "application/json" }), nowPaymentsWebhook);
+subcribedRouter.post("/nowpayments-webhook", express.raw({
+    type: "application/json"
+}), nowPaymentsWebhook);
 
 subcribedRouter.get("/check-subcription", checkSubscription);
 
-subcribedRouter.get("/escort-current-plan", );
+subcribedRouter.get("/escort-current-plan", protect(["Escort"]), fetchEscortCurrentPlan);
 
 
 
