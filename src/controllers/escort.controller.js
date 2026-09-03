afa3b52a2e0af9f5fa8a979316497f36cd76fe71
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-09-03 17:58:57 +0530

    update home page search filter

--- src/controllers/escort.controller.js
+++ src/controllers/escort.controller.js
@@ -3237,7 +3237,7 @@ export async function fetchFilterHomescortscontroller(request, response) {
 
         // City + Additional Cities
 
-        const selectedCity = city?.trim().toUpperCase();
+        const selectedCity = city?.trim().replace(/\s+/g, " ");
 
         if (selectedCity) {
             query.$and.push({
@@ -3259,8 +3259,10 @@ export async function fetchFilterHomescortscontroller(request, response) {
 
 
         if (name?.trim()) {
+            const searchName = name.trim().replace(/\s+/g, " ");
+
             query.name = {
-                $regex: `^${name.trim()}$`,
+                $regex: searchName,
                 $options: "i"
             };
         }
