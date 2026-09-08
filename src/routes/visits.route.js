645c848636d5c5d02a261c3404096198227d9441
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-09-08 17:26:02 +0530

    update visit controller

--- src/routes/visits.route.js
+++ src/routes/visits.route.js
@@ -1,12 +1,19 @@
-import { Router } from "express";
-import { addVisit, getSearchAppearanceStats, getVisitStats, totalVisitStats } from "../controllers/visit.controller.js";
+import {
+    Router
+} from "express";
+import {
+    addVisit,
+    getSearchAppearanceStats,
+    getVisitStats,
+    totalVisitStats
+} from "../controllers/visit.controller.js";
 
 const visitRouter = Router()
 
 visitRouter.post("/add-visit", addVisit);
 visitRouter.get("/fetch-visit-stats", getVisitStats);
 visitRouter.get("/fetch-total-visit-allstats", totalVisitStats);
-visitRouter.post("/get-search-appearance-stats", getSearchAppearanceStats);
+visitRouter.get("/get-search-appearance-stats", getSearchAppearanceStats);
 
 
 
