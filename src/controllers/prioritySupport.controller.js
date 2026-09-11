5c577b9045f435c4d34143cc4b0c6168f0f9ad86
Satish Bhawsar <satishbhawsar92@gmail.com>
2026-09-11 19:41:16 +0530

    update support controller

--- src/controllers/prioritySupport.controller.js
+++ src/controllers/prioritySupport.controller.js
@@ -1,6 +1,8 @@
 import EscortModel from "../models/escortModel.js";
 import PrioritySupportModel from "../models/prioritySupportModel.js";
-import { decrypt } from "../utils/crypto.js";
+import {
+    decrypt
+} from "../utils/crypto.js";
 import {
     sendMail
 } from "../utils/sendMail.js";
